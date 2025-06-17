95c459e5f479662f2b0f83f660101170d54b6680
deltea <thcheetah777@gmail.com>
2025-06-17 19:26:03 +0000

    do a bunch of stuff

--- sketchybar/items/battery.sh
+++ sketchybar/items/battery.sh
@@ -1,6 +1,5 @@
 sketchybar --add item battery right \
-           --subscribe battery system_woke power_source_change \
            --set battery script="$PLUGIN_DIR/battery.sh" \
                          icon.drawing=on \
                          icon.font="JetBrainsMono Nerd Font:Bold:12.0" \
-
+			 update_freq=60
