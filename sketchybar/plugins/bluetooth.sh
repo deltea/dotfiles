4be0f2313452650b62cdf0e60ff7a2e19fd04b2a
deltea <thcheetah777@gmail.com>
2026-06-12 12:37:27 -0700

    remove all skhdrc bindings

--- sketchybar/plugins/bluetooth.sh
+++ sketchybar/plugins/bluetooth.sh
@@ -2,7 +2,7 @@
 
 source "$HOME/.config/icons.sh"
 
-if $(system_profiler SPBluetoothDataType | awk '/Connected:/ {flag=1} /Not Connected:/ {flag=0} flag && /soundcore space one/ {found=1} END {exit !found}'); then
+if $(system_profiler SPBluetoothDataType | awk '/Connected:/ {flag=1} /Not Connected:/ {flag=0} flag && /Nothing Headphone \(1\)/ {found=1} END {exit !found}'); then
   sketchybar --set bluetooth drawing=on
 else
   sketchybar --set bluetooth drawing=off
